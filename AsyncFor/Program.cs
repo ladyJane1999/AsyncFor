9f8ccc1114d647abc64a5809d11cd0fa8e1f4df9
ladyJane1999 <anzhela.romanova.99@bk.ru>
2022-09-20 11:45:25 +0300

    Добавьте файлы проекта.

---
+++ AsyncFor/Program.cs
@@ -0,0 +1,33 @@
+﻿using Nito.AsyncEx;
+class Program
+{
+    static void Main(string[] args)
+    {
+        AsyncContext.Run(() => MainAsync(args));
+    }
+    public static async Task TestAsync()
+    {
+        Console.WriteLine($"T1");
+        Thread.Sleep(1000);
+        Console.WriteLine($"T2");
+        await Task.Delay(1000);
+        Console.WriteLine($"T3");
+    }
+
+    public static async void MainAsync(string[] args)
+    {
+        Func<int, Task> process = x =>
+        {
+            Console.WriteLine($"End processing {x}");
+            return Task.CompletedTask;
+        };
+        int[] array = new int[] { 1, 2, 3, 4, 5 };
+        Parallel.ForEach(array, arr =>
+        {
+            process(arr);
+        });
+        /* foreach(int x in array)
+             await process(x);*/
+        Console.WriteLine($"M2");
+    }
+}
